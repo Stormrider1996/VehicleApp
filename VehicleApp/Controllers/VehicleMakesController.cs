e7becf664bd71ff5be5b3cdfdca64408fcc571cf
Stormrider1996 <76014214+Stormrider1996@users.noreply.github.com>
2022-01-17 10:03:57 +0100

    gotovo

---
+++ VehicleApp/Controllers/VehicleMakesController.cs
@@ -0,0 +1,150 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VehicleApp.Data;
+using VehicleApp.Service;
+
+namespace VehicleApp.Controllers
+{
+    public class VehicleMakesController : Controller
+    {
+        private readonly AppDBContext _context;
+        private readonly IVehicleMakeRepository _IVehicleMakeRepository;
+
+        public VehicleMakesController(AppDBContext context, IVehicleMakeRepository _IVehicleMake)
+        {
+            _context = context;
+            _IVehicleMakeRepository = _IVehicleMake;
+        }
+
+        // GET: VehicleMakes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _IVehicleMakeRepository.GetVehicleMakeListAsync());
+        }
+
+        // GET: VehicleMakes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleMake = await _IVehicleMakeRepository.GetVehicleMakeByID(id);
+            if (vehicleMake == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMakes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleMakes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Abrv")] VehicleMake vehicleMake)
+        {
+            if (ModelState.IsValid)
+            {
+                await _IVehicleMakeRepository.Insert(vehicleMake);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMakes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleMake = await _context.VehicleMakes.FindAsync(id);
+            if (vehicleMake == null)
+            {
+                return NotFound();
+            }
+            return View(vehicleMake);
+        }
+
+        // POST: VehicleMakes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Abrv")] VehicleMake vehicleMake)
+        {
+            if (id != vehicleMake.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _IVehicleMakeRepository.Update(vehicleMake); ;
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleMakeExists(vehicleMake.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMakes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleMake = await _IVehicleMakeRepository.GetVehicleMakeByID(id);
+            if (vehicleMake == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleMake);
+        }
+
+        // POST: VehicleMakes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _IVehicleMakeRepository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleMakeExists(int id)
+        {
+            return _context.VehicleMakes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ VehicleApp/Controllers/VehicleModelsController.cs
@@ -0,0 +1,151 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VehicleApp.Data;
+using VehicleApp.Service;
+
+namespace VehicleApp.Controllers
+{
+    public class VehicleModelsController : Controller
+    {
+        private readonly AppDBContext _context;
+        private readonly IVehicleModelRepository _IVehicleModelRepository;
+
+        public VehicleModelsController(AppDBContext context, IVehicleModelRepository _IVehicleModel)
+        {
+            _context = context;
+            _IVehicleModelRepository = _IVehicleModel;
+        }
+
+
+        // GET: VehicleModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _IVehicleModelRepository.GetVehicleModelListAsync());
+        }
+
+        // GET: VehicleModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleModel = await _IVehicleModelRepository.GetVehicleModelByID(id);
+            if (vehicleModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MakeId,Name,Abrv")] VehicleModel vehicleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await _IVehicleModelRepository.Insert(vehicleModel);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleModel = await _context.VehicleModels.FindAsync(id);
+            if (vehicleModel == null)
+            {
+                return NotFound();
+            }
+            return View(vehicleModel);
+        }
+
+        // POST: VehicleModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MakeId,Name,Abrv")] VehicleModel vehicleModel)
+        {
+            if (id != vehicleModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _IVehicleModelRepository.Update(vehicleModel);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleModelExists(vehicleModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleModel = await _IVehicleModelRepository.GetVehicleModelByID(id);
+            if (vehicleModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleModel);
+        }
+
+        // POST: VehicleModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _IVehicleModelRepository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleModelExists(int id)
+        {
+            return _context.VehicleModels.Any(e => e.Id == id);
+        }
+    }
+}
